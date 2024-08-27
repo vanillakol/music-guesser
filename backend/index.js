e6539ea2cca4c8b475bd607f2ff3673a8b773021
vanillakol <160184591+vanillakol@users.noreply.github.com>
2024-08-27 12:08:34 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -190,7 +190,7 @@ app.use(express.json());
 app.use(
     cors({
         credentials: true,
-        origin: "http://localhost:5173"
+        origin: "https://music-guesser-6her.vercel.app/"
     })
 );
 app.use(cookieParser());
