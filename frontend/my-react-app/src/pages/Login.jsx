5928d401b85759e0f810e2b97300a776d801099f
vanillakol <160184591+vanillakol@users.noreply.github.com>
2024-08-27 13:13:11 -0500

    Update Login.jsx

--- frontend/my-react-app/src/pages/Login.jsx
+++ frontend/my-react-app/src/pages/Login.jsx
@@ -45,7 +45,7 @@ function Login() {
               type="email" 
               id="email" 
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-              placeholder="name@flowbite.com" 
+              placeholder="name@user.com" 
               required 
               onChange={(e) => setData({ ...data, email: e.target.value })} 
               value={data.email} 
